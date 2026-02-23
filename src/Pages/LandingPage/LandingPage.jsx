f28a311ecb1bdf92b460716a8305987d96db4613
FaseehHayat21 <faseehhayat999@gmail.com>
2026-02-23 21:24:31 +0500

    Update

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -4,7 +4,7 @@ import Navbar from '../../Component/FrontPage/Navbar/Navbar'
 import AboutUs from '../../Component/FrontPage/AboutUs/AboutsUs'
 import Services from '../../Component/FrontPage/Services/Services'
 import ProjectOwner from '../../Component/FrontPage/Team/ProjectOwner'
-import Contact from '../../Component/FrontPage/Contact/contact'
+import Contact from '../../Component/FrontPage/Contact/Contact'
 import Footer from '../../Component/FrontPage/Footer/Footer'
 export default function LandingPage() {
   return (
